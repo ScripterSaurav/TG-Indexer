39171c442c8f25352bc46971021044f710387916
NσσႦ Sƈɾιρƚҽɾ <58369054+ScripterSaurav@users.noreply.github.com>
2026-01-07 13:26:44 +0530

    Delete app/views/pages_view.py

--- app/views/pages_view.py
+++
@@ -1,8 +0,0 @@
-import aiohttp_jinja2
-from aiohttp import web
-from .base import BaseView
-
-class DownloadPGView(BaseView):
-    @aiohttp_jinja2.template("downloadPG.html")
-    async def download_pg(self, request):
-        return {}
