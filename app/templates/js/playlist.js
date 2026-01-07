d338f4599790fb1804f20e6f2fbb8c07d92caa99
NσσႦ Sƈɾιρƚҽɾ <58369054+ScripterSaurav@users.noreply.github.com>
2026-01-07 13:04:29 +0530

    Delete app/templates/js/playlist.js

--- app/templates/js/playlist.js
+++
@@ -1,9 +0,0 @@
-function singleItemPlaylist(file,name,basicAuth){
-    let hostUrl = `http://${basicAuth}${window.location.host}`
-    let pd = ""
-    pd += '#EXTM3U\n'
-    pd += `#EXTINF: ${name}\n`
-    pd += `${hostUrl}/${file}\n`
-    let blob = new Blob([pd], { endings: "native" });
-    saveAs(blob, `${name}.m3u`);
-}
