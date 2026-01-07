d8d6ca5175593285cfc6ee78393549d93a7fe8fa
NσσႦ Sƈɾιρƚҽɾ <58369054+ScripterSaurav@users.noreply.github.com>
2026-01-07 13:23:49 +0530

    Update pages_view.py

--- app/views/pages_view.py
+++ app/views/pages_view.py
@@ -2,23 +2,7 @@ import aiohttp_jinja2
 from aiohttp import web
 from .base import BaseView
 
-class ReportView(BaseView):
-    @aiohttp_jinja2.template("video_report.html")
-    async def video_report(self, request):
-        return {} 
-
-class ContactView(BaseView):
-    @aiohttp_jinja2.template("contact.html")
-    async def contact_us(self, request):
-        return {}
-
-class AboutView(BaseView):
-    @aiohttp_jinja2.template("about.html")
-    async def about(self, request):
-        return {} 
-
 class DownloadPGView(BaseView):
     @aiohttp_jinja2.template("downloadPG.html")
     async def download_pg(self, request):
         return {}
-
